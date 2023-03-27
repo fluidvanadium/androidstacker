2737e3a3c5bb304c87a022365120b779af45f70d
fluidvanadium <fluidvanadium@zingolabs.org>
2023-03-27 16:15:04 +0000

    changed setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 
 wget https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip
+unzip commandlinetools-linux-9477386_latest.zip
+mkdir latest
+mv cmdline-tools/* latest
+mv latest cmdline-tools
+export ANDROID_SDK_ROOT=$PWD
+mkdir avds
