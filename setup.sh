3238b529c1ef4cfd89b6cf6f7c95ec6210eebda0
fluidvanadium <fluidvanadium@zingolabs.org>
2023-04-05 16:07:49 +0000

    sdk 30 on x86 build

--- setup.sh
+++ setup.sh
@@ -19,6 +19,8 @@ yes | ./cmdline-tools/latest/bin/sdkmanager --licenses
 ./cmdline-tools/latest/bin/sdkmanager --install cmake
 ./cmdline-tools/latest/bin/sdkmanager --install "system-images;android-29;google_apis_playstore;x86"
 ./cmdline-tools/latest/bin/sdkmanager --install "system-images;android-30;google_apis;x86_64"
+./cmdline-tools/latest/bin/sdkmanager --install "system-images;android-30;google_apis;x86"
 
 ./cmdline-tools/latest/bin/avdmanager create avd --path avd/29_gaps_32 --force --name 29_gaps_32 --package 'system-images;android-29;google_apis_playstore;x86' -d pixel_2
 ./cmdline-tools/latest/bin/avdmanager create avd --path avd/30_ga_64 --force --name 30_ga_64 --package 'system-images;android-30;google_apis;x86_64' -d pixel_6
+./cmdline-tools/latest/bin/avdmanager create avd --path avd/30_ga_32 --force --name 30_ga_32 --package 'system-images;android-30;google_apis;x86' -d pixel_2
