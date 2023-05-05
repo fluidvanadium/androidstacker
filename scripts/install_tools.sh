750c07038504e5502ec8f632b1f863f219925846
fluidvanadium <fluidvanadium@zingolabs.org>
2023-05-05 20:53:34 +0000

    trim script

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,6 +1,5 @@
 yes | ./cmdline-tools/latest/bin/sdkmanager --licenses
 
-./cmdline-tools/latest/bin/sdkmanager --install build-tools
 ./cmdline-tools/latest/bin/sdkmanager --install platform-tools
 ./cmdline-tools/latest/bin/sdkmanager --install emulator
 
