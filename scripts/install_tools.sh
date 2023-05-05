0de566ac1d5f012b7abd5755345d49603607819d
fluidvanadium <fluidvanadium@zingolabs.org>
2023-05-05 20:50:43 +0000

    update

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,5 +1,6 @@
+yes | ./cmdline-tools/latest/bin/sdkmanager --licenses
+
 ./cmdline-tools/latest/bin/sdkmanager --install build-tools
 ./cmdline-tools/latest/bin/sdkmanager --install platform-tools
 ./cmdline-tools/latest/bin/sdkmanager --install emulator
-./cmdline-tools/latest/bin/sdkmanager --install cmake
 
